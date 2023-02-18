daaa795a4628030bbceba818f5d6fdc821c97791
Abiola <olajireeasylife@gmail.com>
2023-02-17 18:36:49 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char b;
+	char c;
+
+	b = 'a';
+	c = 'A';
+	while
+		(b <= 'z') {
+			putchar(b);
+			b++;
+		}
+	while
+		(c <= 'Z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+        return (0);
+}
