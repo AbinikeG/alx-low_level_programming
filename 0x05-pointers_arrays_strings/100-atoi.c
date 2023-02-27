a9dec22bf65938d0f36abb130ddb97506b0a6b90
Abiola <olajireeasylife@gmail.com>
2023-02-27 10:26:03 -0800

    A function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - l -nverts a string to an integer.
+ * _atoi - converts a string to an integer.
  * @s: The string to be converted.
  *
  * Return: The integer value of the converted string.
