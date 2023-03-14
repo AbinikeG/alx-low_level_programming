8d5a7a70dffc0ee5af54e0067c72a18701bfc86b
Abiola <olajireeasylife@gmail.com>
2023-03-13 17:51:44 -0700

    Create the static library libmy.a

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,9 +1,10 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
-char *_strncpy(char *dest, char *src, int n)
+int _putchar(char c)
 {
-    return strncpy(dest, src, n);
+    return putchar(c);
 }
